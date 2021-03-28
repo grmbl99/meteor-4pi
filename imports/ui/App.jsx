8fdb9b03ac4883130c908f57534709f5d3b1bd89
Daan van der Munnik <daan@munnik.net>
2021-03-28 16:40:46 +0200

    added drag and drop

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import { FeaturesCollection } from '/imports/api/FeaturesCollection';
 import { PiView } from './PiView.jsx';
 import { InputForm } from './InputForm.jsx';
@@ -17,21 +19,28 @@ export function App(props) {
     console.log("added: " + input.name);
   };
 
-  function handleRemove(value) {
-    FeaturesCollection.remove({ _id: value});
-    console.log(value);
+  // function handleRemove(value) {
+  //   FeaturesCollection.remove({ _id: value});
+  //   console.log(value);
+  // };
+
+  function handleMove(featureId,pi) {
+    FeaturesCollection.update({ _id: featureId },{ $set: { pi: pi }});
+    console.log("move " + featureId + "to PI" + pi);
   };
 
   return (
-    <div>
+    <DndProvider backend={HTML5Backend}>
       <div>
-        <InputForm onSubmit={handleSubmit}/>
-        <PiView onFeatureClicked={handleRemove} features={features} name="1" />
-        <PiView onFeatureClicked={handleRemove} features={features} name="2" />
-        <PiView onFeatureClicked={handleRemove} features={features} name="3" />
-        <PiView onFeatureClicked={handleRemove} features={features} name="4" />
+        <div>
+          <InputForm onSubmit={handleSubmit}/>
+          <PiView onFeatureDropped={handleMove} features={features} name="1" />
+          <PiView onFeatureDropped={handleMove} features={features} name="2" />
+          <PiView onFeatureDropped={handleMove} features={features} name="3" />
+          <PiView onFeatureDropped={handleMove} features={features} name="4" />
+        </div>
       </div>
-    </div>
+    </DndProvider>
   );  
 }
 
--- imports/ui/PiView.jsx
+++ imports/ui/PiView.jsx
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
+import { useDrag, useDrop } from 'react-dnd'
+import { ItemTypes } from './Constants'
 
 export function PiView(props) {
   const features = props.features;
+
+  const [{isOver}, drop] = useDrop(() => ({
+    accept: ItemTypes.FEATURE,
+    drop: (item) => { props.onFeatureDropped(item.id,props.name) },
+    collect: (monitor) => ({
+      isOver: monitor.isOver()
+    })
+  }))
 
   let size=0;
   features.forEach(feature => {
@@ -12,14 +22,21 @@ export function PiView(props) {
 
   const listItems = features.flatMap((feature) => {
     if (feature.pi === props.name) {
-      return(<Feature onClick={props.onFeatureClicked} key={feature._id} feature={feature} />);
+      return(<Feature key={feature._id} feature={feature} />);
     } else {
       return([]);
     }
   });
 
   return (
-    <div className={size>140 ? "piview pialert" : "piview"}>
+    <div 
+      className="piview"
+      ref={drop}
+      style={{
+        opacity: isOver ? 0.5 : 1, 
+        backgroundColor: size>140 ? "red" : "lightgreen"
+      }}
+    >
       {props.name}: total size: {size}
       {listItems}
     </div>
@@ -29,12 +46,23 @@ export function PiView(props) {
 function Feature(props) {
   const feature=props.feature;
 
-  function onClick() {
-    props.onClick(feature._id);
-  };
+  const [{isDragging}, drag] = useDrag(() => ({
+    type: ItemTypes.FEATURE,
+    item: { id: feature._id },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    })
+  }))
 
   return (
-    <div className="feature" onClick={onClick}>
+    <div 
+      className="feature" 
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        cursor: "move"
+      }}
+    >
       <div className="featurename">Featurename: {feature.name}</div>
       <div className="featuresize">
         <svg width="100px" height="20px">
