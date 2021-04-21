feb3c82093fac470965b88007648443104c36862
Daan van der Munnik <daan@munnik.net>
2021-04-21 11:53:35 +0200

    add indexes on collections

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -1,4 +1,5 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 
 export const FeaturesCollection = new Mongo.Collection('features');
 export const OrgFeaturesCollection = new Mongo.Collection('orgfeatures');
@@ -9,3 +10,11 @@ export const ProjectsCollection = new Mongo.Collection('projects');
 export const AllocationsCollection = new Mongo.Collection('allocations');
 export const VelocitiesCollection = new Mongo.Collection('velocities');
 export const ServerStatusCollection = new Mongo.Collection('serverstatus');
+
+if (Meteor.isServer) {
+  FeaturesCollection._ensureIndex({ id: 1, priority: 1 });
+  OrgFeaturesCollection._ensureIndex({ id: 1 });
+  IterationsCollection._ensureIndex({ sprintName: 1 });
+  TeamsCollection._ensureIndex({ name: 1 });
+  ProjectsCollection._ensureIndex({ name: 1 });
+}
