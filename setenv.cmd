ceb78ad7ac483d4240baab7dedf4b335ea093def
Daan van der Munnik <daan@munnik.net>
2021-05-03 19:47:41 +0200

    add windows setenv script

---
+++ setenv.cmd
@@ -0,0 +1,3 @@
+@REM this script sets the METEOR_SETTINGS environment variable with the content of settings.json
+@REM on windows, make sure to remove newlines and whitespace from settings.json
+set /P METEOR_SETTINGS=<settings.json
